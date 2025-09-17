14af5d5f048bf539b94d6563258bd176aaf02e1a
cyrus-trixie <125303529+cyrus-trixie@users.noreply.github.com>
2025-09-17 12:00:52 +0300

    vite file

--- MyReact/vite.config.js
+++ MyReact/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This ensures that all routes fall back to index.html for client-side routing.
+    historyApiFallback: true,
+  },
 })
